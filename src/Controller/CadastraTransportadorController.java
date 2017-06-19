98b1ca04509a7f4aa93a6bb2d633586f0e788526
Aluno <Aluno@172.16.0.217>
2017-06-19 09:02:54 -0300

    Add tela de transportadora

--- src/Controller/CadastraTransportadorController.java
+++ src/Controller/CadastraTransportadorController.java
@@ -10,24 +10,33 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-
-
-public class LoginController implements Initializable {
-    
-    @FXML
-    private Button btn_Logar;
-
-    @FXML
-    private TextField tfLogin_login;
-
-    @FXML
-    private TextField tfSenha_login;
+/**
+ * FXML Controller class
+ *
+ * @author Aluno
+ */
+public class CadastraTransportadorController implements Initializable {
 
     
+    @FXML
+    private TextField tfContato;
+
+    @FXML
+    private Button btnCadastrar;
+
+    @FXML
+    private TextField tfCnpj;
+
+    @FXML
+    private TextField tfNomeTransportadora;
+
+    @FXML
+    private ComboBox<?> comboTipoMaterial;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        btn_Logar.getStylesheets().add(getClass().getResource("/View/newCascadeStyleSheet.css").toExternalForm());
+        // TODO
     }    
     
 }
